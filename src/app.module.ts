06a2e4e445cafdc48144dd28f418c89a02972f3c
George Andjic <djordjeandjic99@gmail.com>
2022-06-23 22:35:23 +0300

    Created CRUD resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemModule } from './item/item.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [],
+  imports: [ItemModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
